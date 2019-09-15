9233748e614b1cc3108917e0052b80b10ffb07a0
flansel <fjansell@gmail.com>
2019-09-15 00:05:19 -0400

    bugs fixed

--- boxes.c
+++ boxes.c
@@ -9,12 +9,8 @@ int main(int argc, char* argv[]){
 	int dim;
 	printf("enter Number of boxes and the demensions\n");
 	scanf("%d %d", &number, &dim);
-	int** box;
+	int box [number][dim];
 	int i,j;
-	box = malloc(number * sizeof *box);
-	for(i = 0;i<number;i++){
-		box[i] = malloc(dim * sizeof *box[i]);
-	}
   	for(i = 0; i < number; i++){
 		printf("enter box %d\n", i+1);
     	for(j = 0; j < dim; j++){
@@ -27,20 +23,22 @@ int main(int argc, char* argv[]){
 
 void arrSort(int* arr, int number, int dim){
 	int i,j;
-	printf("%p", arr);
-	for(i = 0; i < number; i++){
-     	for(j = 0; j < dim; j++){
-			printf("%p\n", (arr+(i*dim)+j));
-     	}
-     	printf("\n");
-  	}
 	for(i = 0;i<number;i++){
 		for(j = 0;j<dim-1;j++){
 			if(*((arr+i*dim) + j) > *((arr+i*dim) +j+1)){
 				int temp = *((arr+i*dim) + j);
-				*((arr+i*dim) + j) = *((arr+i*dim) + j+1);
-				*((arr+i*dim) + j+1) = temp;
+				int* p1 = ((arr+i*dim)+j);
+				int* p2 = ((arr+i*dim)+j+1);
+				*p1  = *((arr+i*dim) + j+1);
+				*p2  = temp;
 			}
 		}
+	}
+
+	for(i = 0;i<number;i++){
+		for(j = 0;j<dim;j++){
+			printf("%d ", *((arr + i*dim)+j));
+		}
+		printf("\n");
 	}
 }
